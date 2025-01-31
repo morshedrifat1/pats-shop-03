6dc521fad42c446b5bb50fc6890f7d56ef11ac46
morshedrifat1 <morshedrifat0@gmail.com>
2025-01-31 22:42:20 +0600

    new section add

---
+++ castom-tailwind.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "primary": '#F9DF8A',
+        "secondary": "#FFF9F4",
+        "btn-bg":"#FFDC26",
+        "tx-primary": "#111111"
+      },
+      fontFamily: {
+        "adlam": ["ADLaM Display"],
+        "sora":["Sora"]
+      },
+    },
+  },
+};
